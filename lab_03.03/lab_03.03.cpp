e2ad71d63971baaf4d26137b6ad37cf6d17a6056
Andriy Chykerechko Jr <andrewchiker@gmail.com>
2022-10-25 14:57:03 +0300

    lab

---
+++ lab_03.03/lab_03.03.cpp
@@ -0,0 +1,37 @@
+﻿// Lab_03_3.cpp
+// <Чикеречко Андрій>
+// Лабораторна робота № 3.3
+// Розгалуження, задане графіком функції.
+// Варіант 19
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    double x, R;
+    cout << "R = ";
+    cin >> R;
+    cout << "x = ";
+    cin >> x;
+
+    double y;
+    if (x < -1) {
+        y = x * (-1) - 1;
+    }
+    else if (x >= -1 && x <= 1) {
+        y = 0;
+    }
+    else if (x > 1 && x <= (2 * R + 1)) {
+        y = sqrt((2 * R - x + 1) * (x - 1));
+    }
+    else {
+        y = (2 * R - x + 1) / (6 - 2 * R);
+    }
+
+    cout << endl;
+    cout << "y = " << y << endl;
+
+    return 0;
+}
